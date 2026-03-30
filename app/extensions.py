69a452d9bc40f4e3fbb740c8891a2f7ee3f86e12
nabeelrehman120604 <nabeelrehman1276@gmail.com>
2026-03-31 01:04:20 +0530

    checking status of merge

--- app/extensions.py
+++ app/extensions.py
@@ -8,6 +8,8 @@ login_manager = LoginManager()
 bcrypt = Bcrypt()
 csrf = CSRFProtect()
 
+
+
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'warning'
